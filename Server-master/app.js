5fcc5911fb8b4534d3cd85946ed76170c1d18729
Nayanthara_D <nayanthara@lidexglobal.com>
2023-02-24 20:05:49 +0530

    build

--- Server-master/app.js
+++ Server-master/app.js
@@ -180,12 +180,12 @@ app.listen(3001, () => {
 })
 
 // ...............................................................................................................
-app.use(express.static(path.join(__dirname, "front_end-master\build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "front_end-master\build\index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
-});
+// app.use(express.static(path.join(__dirname, "front_end-master\build")));
+// app.get("*", function (_, res) {
+//   res.sendFile(
+//     path.join(__dirname, "front_end-master\build\index.html"),
+//     function (err) {
+//       res.status(500).send(err);
+//     }
+//   );
+// });
